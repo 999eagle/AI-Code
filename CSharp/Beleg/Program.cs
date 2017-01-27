b63109fce6786342a81d2e13fc798d946c1b888c
Erik Tauchert <erik@tauchert.de>
2017-01-27 23:44:35 +0100

    remove old code, add more doc

--- CSharp/Beleg/Program.cs
+++ CSharp/Beleg/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Beleg
 {
+	/// <summary>
+	/// Main class.
+	/// </summary>
 	class Program
 	{
+		/// <summary>
+		/// Main entry point for the program.
+		/// </summary>
+		/// <param name="args">Command line arguments. Not used.</param>
 		static void Main(string[] args)
 		{
 			var problem = new QueenProblem.Problem(5);
